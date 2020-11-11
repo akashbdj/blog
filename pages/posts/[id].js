708fe4f92fe8d13d1c109bd95a3924a14c78511d
akashbdj <akashbdj@gmail.com>
2020-11-11 17:20:00 +0530

    syntax highlight

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,24 +3,34 @@ import Date from '../../components/date'
 import Link from 'next/link'
 import Head from 'next/head'
 import Layout from '../../components/layout'
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { nord } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+const renderers = {
+    code: ({ language, value }) => {
+        return <SyntaxHighlighter style={nord} language={language} children={value} />
+    }
+}
 
 export default function Post({ postData }) {
+    const { frontmatter, content } = postData
     return (
         <Layout>
             <Head>
-                <title>{postData.title}</title>
+                <title>{frontmatter.title}</title>
             </Head>
             <div className='text-center mt-4'>
                 <a href='/'><i className='icofont-deer-head icofont-3x'></i></a>
             </div>
-            <article className='flex flex-col mt-10'>
+            <article className='flex flex-col my-10'>
                 <header className='my-8'>
-                    <h1 className='font-semibold text-3xl'>{postData.title}</h1>
+                    <h1 className='font-semibold text-3xl'>{frontmatter.title}</h1>
                     <div className={'text-xs text-gray-900'}>
-                        <Date dateString={postData.date} />
+                        <Date dateString={frontmatter.date} />
                     </div>
                 </header>
-                <div className='markdown text-xs' dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+                <ReactMarkdown source={content} renderers={renderers} className='markdown text-xs' />
             </article>
         </Layout>
     )
